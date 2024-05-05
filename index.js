0e4f162bd66e6667649b34643db069b6af5dca80
sedrftgyu8 <gs1rarar121@hotmail.com>
2024-05-05 20:43:33 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function generateParenthesis(n) {
-  const result = [];
-  backtrack("", 0, 0);
-  return result;
-  function backtrack(current, open, close) {
-    if (current.length === 2 * n) result.push(current);
-    if (open < n) backtrack(current + "(", open + 1, close);
-    if (close < open) backtrack(current + ")", open, close + 1);
-  }
-}
+const flattenDepth = (arr, depth = 1) =>
+  arr.reduce(
+    (a, v) =>
+      a.concat(depth > 1 && Array.isArray(v) ? flattenDepth(v, depth - 1) : v),
+    [],
+  );
