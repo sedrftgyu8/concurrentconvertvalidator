ebb15fff0befba61609380021b909ae72a738d8e
sedrftgyu8 <gs1rarar121@hotmail.com>
2024-05-09 06:04:32 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,26 +1,2 @@
-const mergeSortIterative = (arr) => {
-  const merge = (left, right) => {
-    let result = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-      if (left[leftIndex] < right[rightIndex]) {
-        result.push(left[leftIndex]);
-        leftIndex++;
-      } else {
-        result.push(right[rightIndex]);
-        rightIndex++;
-      }
-    }
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-  };
-  const mergeSize = 2;
-  for (let i = 0; i < arr.length; i += mergeSize) {
-    for (let j = i; j < arr.length; j += mergeSize) {
-      const left = arr.slice(j, j + mergeSize / 2);
-      const right = arr.slice(j + mergeSize / 2, j + mergeSize);
-      arr.splice(j, mergeSize, ...merge(left, right));
-    }
-  }
-  return arr;
-};
+const titleCase = (str) =>
+  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
