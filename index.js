eb517699b4bf4de014fb2a8734fda4bdf972bf1d
sedrftgyu8 <gs1rarar121@hotmail.com>
2024-05-10 00:38:36 +0700

    update task

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const titleCase = (str) =>
-  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
